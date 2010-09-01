9d0922d487bb4252ddde2f7a0800f831b3a6d42f
David Golden <dagolden@cpan.org>
2010-09-01 16:19:54 -0400

    initial import of CPAN-Testers-Metabase-Feed

---
+++ lib/CPAN/Testers/Metabase/Feed.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package CPAN::Testers::Metabase::Feed;
+# ABSTRACT: No abstract given for CPAN::Testers::Metabase::Feed
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use [% module %];
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
